aa4d377595aa6c3a50397e9424f4b828d11e3108
Hari <iamhari1234@gmail.com>
2018-05-31 09:10:10 +0530

    Adding thymeleaf to boot

---
+++ boot/src/main/java/spring/boot/controller/PublisherController.java
@@ -0,0 +1,21 @@
+package spring.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.boot.repository.PublisherRepository;
+
+@Controller
+public class PublisherController {
+    private PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @RequestMapping("/publishers")
+    public String getPublishers(Model model){
+        model.addAttribute("publishers",publisherRepository.findAll());
+        return "publishers";
+    }
+}
